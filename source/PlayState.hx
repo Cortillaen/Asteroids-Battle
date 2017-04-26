f911f7af116c3ebe014bc73b97ca765a3d970398
Cortillaen <senkairyu@gmail.com>
2017-04-26 17:06:48 -0500

    The other half of changing where control keys are decided

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,13 +1,16 @@
 package;
 
 import flixel.FlxState;
+import flixel.input.keyboard.FlxKey;
 
 class PlayState extends FlxState
 {
+	static private var P1_KEYS(default, never):Array<Array<FlxKey>> = [[FlxKey.W], [FlxKey.A], [FlxKey.D], [FlxKey.CONTROL, FlxKey.SPACE]];
+	
 	override public function create():Void
 	{
 		super.create();
-		add(new Player(200, 200));
+		add(new Player(200, 200, P1_KEYS));
 	}
 
 	override public function update(elapsed:Float):Void
