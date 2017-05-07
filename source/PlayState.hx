d8027a8dc1e034dcde7e2173a7e59b90bd58df33
Cortillaen <senkairyu@gmail.com>
2017-05-07 00:01:22 -0500

    Player being destroyed by asteroid ends game

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,11 +7,13 @@ import flixel.input.keyboard.FlxKey;
 import flixel.math.FlxAngle;
 import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	static private var P1_KEYS(default, never):Array<Array<FlxKey>> = [[FlxKey.W], [FlxKey.A], [FlxKey.D], [FlxKey.CONTROL, FlxKey.SPACE]];
 	static private var P2_KEYS(default, never):Array<Array<FlxKey>> = [[FlxKey.NUMPADEIGHT], [FlxKey.NUMPADFOUR], [FlxKey.NUMPADSIX], [FlxKey.NUMPADZERO, FlxKey.ENTER]];
+	private var gameOver:Bool;
 	public var players:FlxTypedGroup<Player>;
 	public var bullets:FlxTypedGroup<Bullet>;
 	public var asteroids:FlxTypedGroup<Asteroid>;
@@ -38,6 +40,12 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		if (gameOver && FlxG.keys.anyPressed([FlxKey.R])) {
+			FlxG.switchState(new MenuState());
+		}
+		if (asteroids.countLiving() < 3) {
+			asteroids.recycle(Asteroid);
+		}
 		FlxG.overlap(players, asteroids, function(p:Player, a:Asteroid){
 			var retColor:FlxColor = a.impact(p);
 			if (retColor == FlxColor.GREEN) {
@@ -59,6 +67,12 @@ class PlayState extends FlxState
 				//nothing happens
 			}
 			else { //game over
+				players.kill();
+				var showText = new FlxText(0, 100, FlxG.width, (retColor == FlxColor.RED ? "RED" : "BLUE") + " Player Wins!\n\nPress R to return to menu.", 40);
+				showText.alignment = FlxTextAlign.CENTER;
+				showText.addFormat(new FlxTextFormat(retColor, true, false, FlxColor.BLACK));
+				add(showText);
+				gameOver = true;
 			}
 		});
 		FlxG.overlap(bullets, asteroids, function(b:Bullet, a:Asteroid){a.impact(b); });
