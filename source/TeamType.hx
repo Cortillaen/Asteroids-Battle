6b3bce8454c7e259a11c532fca6ecd0c7dccdc33
Cortillaen <senkairyu@gmail.com>
2017-05-10 16:21:06 -0500

    Fixed offscreen space ignoring size of different sprites

--- source/TeamType.hx
+++ source/TeamType.hx
@@ -14,17 +14,17 @@ enum TeamType {
 
 class AstUtil {
 	static public function switchSides(obj:FlxSprite) {
-		if (obj.x < (0 - 100 * obj.scale.x *1.1)) {
-			obj.x = FlxG.width + 100 * obj.scale.x *0.1;
+		if (obj.x < (0 - obj.graphic.width * obj.scale.x * 1.1)) {
+			obj.x = FlxG.width + obj.graphic.width * obj.scale.x * 0.1;
 		}
-		else if (obj.x > (FlxG.width + 100 * obj.scale.x *0.1)) {
-			obj.x = (0 - 100 * obj.scale.x *1.1);
+		else if (obj.x > (FlxG.width + obj.graphic.width * obj.scale.x * 0.1)) {
+			obj.x = (0 - obj.graphic.width * obj.scale.x * 1.1);
 		}
-		if (obj.y < (0 - 100 * obj.scale.y *1.1)) {
-			obj.y = FlxG.height + 100 * obj.scale.y *0.1;
+		if (obj.y < (0 - obj.graphic.height * obj.scale.y * 1.1)) {
+			obj.y = FlxG.height + obj.graphic.height * obj.scale.y * 0.1;
 		}
-		else if (obj.y > (FlxG.height + 100 * obj.scale.y *0.1)) {
-			obj.y = (0 - 100 * obj.scale.y *1.1);
+		else if (obj.y > (FlxG.height + obj.graphic.height * obj.scale.y * 0.1)) {
+			obj.y = (0 - obj.graphic.height * obj.scale.y * 1.1);
 		}
 	}
 }
